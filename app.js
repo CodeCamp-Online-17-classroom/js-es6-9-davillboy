cd9985993c6d47d6e8041488110f3bd9921a4ef2
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-24 16:45:53 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // write code here
+function cloneArray(arr) {
+  return arr.slice();
+}
+
+const originalArray = [1, 2, 3, 4, 5];
+const clonedArray = cloneArray(originalArray);
+
+console.log(originalArray); 
+console.log(clonedArray); 
